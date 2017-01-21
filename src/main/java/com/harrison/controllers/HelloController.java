bcfd6050f8ac4e90954eedc3a66f11bdec9dc6a0
Harrison Hocker <hjhocker@gmail.com>
2017-01-21 09:08:33 -0600

    first

---
+++ src/main/java/com/harrison/controllers/HelloController.java
@@ -0,0 +1,16 @@
+package com.harrison.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/hello")
+public class HelloController {
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String sayHello() {
+		return "Hello";
+	}
+
+}
