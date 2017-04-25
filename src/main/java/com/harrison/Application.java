9a18e1da9f07f548a92d1ab71b21207c70f45f69
Harrison Hocker <hjhocker@gmail.com>
2017-04-25 18:35:23 -0500

    test

--- src/main/java/com/harrison/Application.java
+++ src/main/java/com/harrison/Application.java
@@ -2,6 +2,7 @@ package com.harrison;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
@@ -11,5 +12,5 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-	
+
 }
