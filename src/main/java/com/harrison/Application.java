fe9951fd94843394105f1ff3c6615afd6f255860
Harrison Hocker <hjhocker@gmail.com>
2017-04-24 21:50:13 -0500

    thig

--- src/main/java/com/harrison/Application.java
+++ src/main/java/com/harrison/Application.java
@@ -1,6 +1,7 @@
 package com.harrison;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -11,5 +12,5 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-	
+
 }
