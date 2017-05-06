ae8379bfb6e582dc65c531d3c6d3fb3f7e091ca9
Harrison Hocker <hjhocker@gmail.com>
2017-05-06 14:21:31 -0500

    again

--- src/main/java/com/harrison/controllers/EventController.java
+++ src/main/java/com/harrison/controllers/EventController.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
