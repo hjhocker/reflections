ef2de9fccbb8f269644793c3ef36a2bb7560055b
Harrison <hjhocker@gmail.com>
2017-05-30 19:35:38 -0500

    setup cors for local and aws (#32)

---
+++ src/main/java/com/harrison/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.harrison.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Component
+public class CorsConfig {
+
+    @Value("${reflections.hostname}")
+    private String reflectionsHostname;
+    
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**").allowedOrigins(reflectionsHostname);
+            }
+        };
+    }
+    
+}
