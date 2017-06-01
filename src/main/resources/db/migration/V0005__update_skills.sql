47ab41335764ab8918b2b057d3d39fae49d37789
Harrison Hocker <hjhocker@gmail.com>
2017-05-31 19:30:47 -0500

    updated skills migration

---
+++ src/main/resources/db/migration/V0005__update_skills.sql
@@ -0,0 +1,12 @@
+delete from skill;
+
+insert into skill (name, years_of_experience, proficiency)
+  values
+  ('Git/GitHub', 3, 'INTERMEDIATE'),
+  ('Jenkins',	1, 'BEGINNER'),
+  ('Sonar',	1, 'BEGINNER'),
+  ('Java',	3, 'ADVANCED'),
+  ('Linux',	10, 'ADVANCED'),
+  ('Bash',	10,	'ADVANCED'),
+  ('Angular',	1, 'BEGINNER'),
+  ('SQL',	3, 'INTERMEDIATE');
