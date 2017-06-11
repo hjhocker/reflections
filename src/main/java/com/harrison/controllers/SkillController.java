d1784a62f27d93c940754515243ead8217319c49
Harrison Hocker <hjhocker@gmail.com>
2017-06-11 11:05:39 -0500

    cleanup

--- src/main/java/com/harrison/controllers/SkillController.java
+++ src/main/java/com/harrison/controllers/SkillController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,11 +29,6 @@ public class SkillController {
     @RequestMapping(value = "/{name}", method = RequestMethod.GET)
     public ResponseEntity<Skill> getSkill(@PathVariable("name") String name) {
         return new ResponseEntity<>(skillRepository.findByName(name), HttpStatus.OK);
-    }
-
-    @RequestMapping(method = RequestMethod.POST)
-    public ResponseEntity<Skill> createSkill(@RequestBody Skill skill) {
-        return new ResponseEntity<>(skillRepository.save(skill), HttpStatus.OK);
     }
 
     @RequestMapping(value = "/{name}", method = RequestMethod.DELETE)
