fdf3470031c2df2d3ec0363a9703346aae996e36
Harrison Hocker <hjhocker@gmail.com>
2017-06-16 07:44:48 -0500

    low level jdbc access for suggested skills

--- src/main/java/com/harrison/controllers/SuggestedSkillsController.java
+++ src/main/java/com/harrison/controllers/SuggestedSkillsController.java
@@ -1,27 +1,61 @@
 package com.harrison.controllers;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.harrison.enums.Proficiency;
 import com.harrison.suggestedskills.domain.SuggestedSkill;
-import com.harrison.suggestedskills.repository.SuggestedSkillRepository;
 
 @RestController
 @RequestMapping(value = "/api/suggested/skills")
 public class SuggestedSkillsController {
 
     @Autowired
-    private SuggestedSkillRepository suggestedSkillRepository;
+    @Qualifier("suggestedSkillsDataSource")
+    private DataSource suggestedSkillsDataSource;
 
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<List<SuggestedSkill>> getSkills() {
-        return new ResponseEntity<>(suggestedSkillRepository.findAll(), HttpStatus.OK);
+    public ResponseEntity<List<SuggestedSkill>> getSkills() throws SQLException {
+        String sql = "select * from suggested_skill";
+        ResultSet rs;
+        Connection conn = null;
+        PreparedStatement ps = null;
+        try {
+            conn = suggestedSkillsDataSource.getConnection();
+            ps = conn.prepareStatement(sql);
+            try {
+                rs = ps.executeQuery();
+            } finally {
+                ps.close();
+            }
+        } finally {
+            if (conn != null) {
+                conn.close();
+            }
+        }
+        List<SuggestedSkill> list = new ArrayList<>(rs.getFetchSize());
+        while (rs.next()) {
+            SuggestedSkill skill = new SuggestedSkill();
+            skill.setName(rs.getString("name"));
+            skill.setYearsOfExperience(Float.valueOf(rs.getString("years_of_experience")));
+            skill.setProficiency(Proficiency.valueOf(rs.getString("proficiency")));
+            list.add(skill);
+        }
+        return new ResponseEntity<>(list, HttpStatus.OK);
     }
 
 }
