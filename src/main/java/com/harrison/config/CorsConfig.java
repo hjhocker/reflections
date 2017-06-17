a24c8cdcb6694f302e977cf36e69d6a2f9e9e25d
Harrison Hocker <hjhocker@gmail.com>
2017-06-17 11:51:02 -0500

    simplify

--- src/main/java/com/harrison/config/CorsConfig.java
+++ src/main/java/com/harrison/config/CorsConfig.java
@@ -2,15 +2,13 @@ package com.harrison.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Component
-@Profile("aws")
-public class AwsCorsConfig {
+public class CorsConfig {
 
     @Value("${reflections.hostname}")
     private String[] reflectionsHostnames;
