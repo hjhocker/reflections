e09345513ba2a741a584b01164d54bed072aa923
Harrison Hocker <hjhocker@gmail.com>
2017-06-16 21:57:00 -0500

    configure flyway for aws

--- src/main/java/com/harrison/config/AwsReflectionsConfig.java
+++ src/main/java/com/harrison/config/AwsReflectionsConfig.java
@@ -1,11 +1,15 @@
 package com.harrison.config;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 import javax.sql.DataSource;
 
+import org.flywaydb.core.Flyway;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.flyway.FlywayDataSource;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
@@ -26,6 +30,18 @@ public class AwsReflectionsConfig {
     
     @Autowired
     private Environment env;
+    
+    @Bean(initMethod = "migrate")
+    @Primary
+    @FlywayDataSource
+    public Flyway reflectionsFlyway() throws IOException {
+        Flyway flyway = new Flyway();
+        Properties properties = new Properties();
+        flyway.setLocations("classpath:db/migration/reflections");
+        flyway.configure(properties);
+        flyway.setDataSource(reflectionsDataSource());
+        return flyway;
+    }
     
     @Bean(name = "reflectionsJdbcTemplate")
     public JdbcTemplate reflectionsJdbcTemplate() {
