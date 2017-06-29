d7ab3d6a3f9c20d64ea378a657669aaea075689b
Harrison <hjhocker@gmail.com>
2017-06-28 20:51:56 -0500

    grad school skills (#45)

---
+++ src/main/resources/db/migration/reflections/V0008__grad_school_skills.sql
@@ -0,0 +1,7 @@
+insert into skill (name, years_of_experience, proficiency)
+  values
+  ('Tcl/Tk',	5, 'INTERMEDIATE'),
+  ('Autodock', 5, 'ADVANCED'),
+  ('Autodock Vina', 3, 'INTERMEDIATE'),
+  ('Visual Molecular Dynamic (VMD)', 5, 'INTERMEDIATE'),
+  ('NAMD - Scalable Molecular Dynamics', 4, 'INTERMEDIATE');
