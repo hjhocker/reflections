6f0da8db1d47cdf47c17db88a27fa579b8792f1f
Harrison Hocker <hjhocker@gmail.com>
2017-07-08 10:24:13 -0500

    sql and test transaction manager

--- src/main/java/com/harrison/controllers/SkillController.java
+++ src/main/java/com/harrison/controllers/SkillController.java
@@ -3,8 +3,12 @@ package com.harrison.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,6 +23,10 @@ public class SkillController {
 
     @Autowired
     private SkillRepository skillRepository;
+    
+    @Autowired
+    @Qualifier("suggestedSkillsPlatformTransactionManager")
+    private PlatformTransactionManager transactionManager;
 
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<List<Skill>> getSkills() {
@@ -27,6 +35,38 @@ public class SkillController {
 
     @RequestMapping(value = "/{name}", method = RequestMethod.GET)
     public ResponseEntity<Skill> getSkill(@PathVariable("name") String name) {
+        TransactionStatus trans = transactionManager.getTransaction(new TransactionDefinition() {
+            
+            @Override
+            public boolean isReadOnly() {
+                // TODO Auto-generated method stub
+                return false;
+            }
+            
+            @Override
+            public int getTimeout() {
+                // TODO Auto-generated method stub
+                return 0;
+            }
+            
+            @Override
+            public int getPropagationBehavior() {
+                // TODO Auto-generated method stub
+                return 0;
+            }
+            
+            @Override
+            public String getName() {
+                // TODO Auto-generated method stub
+                return null;
+            }
+            
+            @Override
+            public int getIsolationLevel() {
+                // TODO Auto-generated method stub
+                return 0;
+            }
+        });
         return new ResponseEntity<>(skillRepository.findByName(name), HttpStatus.OK);
     }
 
