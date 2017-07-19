e9e82caeba37dd5e1a2df712e4b0d2985767b977
Harrison Hocker <hjhocker@gmail.com>
2017-07-19 18:03:31 -0500

    controllers

---
+++ src/main/java/com/harrison/controllers/DataSetController.java
@@ -0,0 +1,28 @@
+package com.harrison.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.harrison.ai.domain.IrisEntity;
+import com.harrison.ai.repository.IrisRepository;
+
+
+@RestController
+@RequestMapping("/api/data")
+public class DataSetController {
+
+    @Autowired
+    private IrisRepository irisRepository;
+    
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<IrisEntity>> getIrisData() {
+        return new ResponseEntity<>(irisRepository.findAll(), HttpStatus.OK);
+    }
+    
+}
