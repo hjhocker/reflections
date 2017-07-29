7649d05f08f1ddf0c450a7f3d0e3e8c5785fcbbd
Harrison Hocker <hjhocker@gmail.com>
2017-07-29 12:57:57 -0500

    sonar

--- src/main/java/com/harrison/messaging/Consumer.java
+++ src/main/java/com/harrison/messaging/Consumer.java
@@ -1,14 +1,18 @@
 package com.harrison.messaging;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Consumer {
     
+    private static final Logger LOGGER = LoggerFactory.getLogger(Consumer.class);
+    
     @RabbitListener(queues = "hello")
     public void receive(String in) {
-        System.out.println(" [x] Received '" + in + "'");
+        LOGGER.info(" [x] Received '" + in + "'");
     }
 
 }
