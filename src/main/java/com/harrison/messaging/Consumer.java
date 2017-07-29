dc86a3a1fe071d85409381e4905cfc72c0073f8d
Harrison Hocker <hjhocker@gmail.com>
2017-07-29 12:44:16 -0500

    python puts data onto the queue as ASCII. java api seems to use strings

--- src/main/java/com/harrison/messaging/Consumer.java
+++ src/main/java/com/harrison/messaging/Consumer.java
@@ -8,6 +8,12 @@ public class Consumer {
     
     @RabbitListener(queues = "hello")
     public void receive(String in) {
+//        String[] arr = in.split(",");
+//        for (String a : arr) {
+//            int i = Integer.valueOf(a);
+//            char fff = (char) i;
+//            System.out.println(fff);
+//        }
         System.out.println(" [x] Received '" + in + "'");
     }
 
