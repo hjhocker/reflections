80e306604c05f4d451f566750f6292c8bbea67a0
Harrison Hocker <hjhocker@gmail.com>
2017-11-19 10:43:24 -0600

    build custom document

--- src/main/java/com/harrison/controllers/EmployeeController.java
+++ src/main/java/com/harrison/controllers/EmployeeController.java
@@ -2,6 +2,7 @@ package com.harrison.controllers;
 
 import java.io.ByteArrayInputStream;
 
+import com.aspose.words.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,8 +13,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.aspose.cells.Workbook;
-import com.aspose.words.Document;
-import com.aspose.words.SaveOutputParameters;
 import com.harrison.reflections.domain.EmployeeRead;
 import com.harrison.reflections.domain.EmployeeWrite;
 import com.harrison.reflections.repository.EmployeeReadRepository;
@@ -46,12 +45,87 @@ public class EmployeeController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
     
+    
+    
     @RequestMapping(value = "/report", method = RequestMethod.POST)
     public ResponseEntity<SaveOutputParameters> doStuff(@RequestBody  byte[] document) throws Exception {
         Document doc = new Document(new ByteArrayInputStream(document));
 //        Document doc = new Document("/tmp/input.docx");
         SaveOutputParameters parameters = doc.save("/tmp/out.pdf");
+        toc();
         return new ResponseEntity<>(parameters, HttpStatus.OK);
+    }
+    
+    public void toc() throws Exception {
+        Document doc = new Document();
+        DocumentBuilder builder = new DocumentBuilder(doc);
+         
+        // Insert a table of contents at the beginning of the document.
+        builder.insertTableOfContents("\\o \"1-3\" \\h \\z \\u");
+         
+        // The newly inserted table of contents will be initially empty.
+        // It needs to be populated by updating the fields in the document.
+        doc.updateFields();
+        String dataDir = "/tmp/";
+        doc.save(dataDir + "AsposeTableOfContents.doc");
+         
+        // ==================================
+        // Table Of Contents with Headings
+        // ==================================
+         
+        Document doc1 = new Document();
+         
+        // Create a document builder to insert content with into document.
+        builder = new DocumentBuilder(doc1);
+
+        Style harrisonStyle = doc1.getStyles().add(StyleType.PARAGRAPH, "Harrison Style");
+        harrisonStyle.getFont().setName("Verdana");
+        harrisonStyle.getFont().setSize(14);
+
+        // Insert a table of contents at the beginning of the document.
+        builder.insertTableOfContents("\\o \"1-3\" \\h \\z \\u");
+         
+        // Start the actual document content on the second page.
+        builder.insertBreak(BreakType.PAGE_BREAK);
+         
+        // Build a document with complex structure by applying different heading styles thus creating TOC entries.
+        builder.getParagraphFormat().setStyleIdentifier(StyleIdentifier.HEADING_1);
+         
+        builder.writeln("Heading 1");
+         
+        builder.getParagraphFormat().setStyleIdentifier(StyleIdentifier.HEADING_2);
+
+        builder.writeln("Heading 1.1");
+        builder.writeln("Heading 1.2");
+         
+        builder.getParagraphFormat().setStyleIdentifier(StyleIdentifier.HEADING_1);
+         
+        builder.writeln("Heading 2");
+        builder.writeln("Heading 3");
+         
+        builder.getParagraphFormat().setStyleIdentifier(StyleIdentifier.HEADING_2);
+         
+        builder.writeln("Heading 3.1");
+         
+        builder.getParagraphFormat().setStyleIdentifier(StyleIdentifier.HEADING_3);
+         
+        builder.writeln("Heading 3.1.1");
+
+            builder.getParagraphFormat().setStyle(harrisonStyle);
+            builder.writeln("subsection text with a cool style");
+
+        builder.getParagraphFormat().setStyleIdentifier(StyleIdentifier.HEADING_3);
+        builder.writeln("Heading 3.1.2");
+        builder.writeln("Heading 3.1.3");
+         
+        builder.getParagraphFormat().setStyleIdentifier(StyleIdentifier.HEADING_2);
+         
+        builder.writeln("Heading 3.2");
+        builder.writeln("Heading 3.3");
+         
+        // Call the method below to update the TOC.
+        doc1.updateFields();
+        doc1.save(dataDir + "AsposeTOCHeadings.doc");
     }
     
 }
