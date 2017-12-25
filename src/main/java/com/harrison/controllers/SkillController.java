a1aea2e4d7493626d3803ad2a6d3481b3b2a4f67
Harrison Hocker <hjhocker@gmail.com>
2017-12-25 08:30:25 -0600

    working example of entity manager for multiple persistence units

--- src/main/java/com/harrison/controllers/SkillController.java
+++ src/main/java/com/harrison/controllers/SkillController.java
@@ -2,6 +2,8 @@ package com.harrison.controllers;
 
 import java.util.List;
 
+import com.harrison.reflections.domain.Name;
+import com.harrison.suggestedskills.domain.SuggestedSkill;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
@@ -18,6 +20,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.harrison.reflections.domain.Skill;
 import com.harrison.reflections.repository.SkillRepository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 @RestController
 @RequestMapping(value = "/api/skills")
 public class SkillController {
@@ -29,6 +34,12 @@ public class SkillController {
     @Qualifier("reflectionsPlatformTransactionManager")
     private PlatformTransactionManager transactionManager;
 
+    @PersistenceContext(name = "reflections", unitName = "reflections")
+    private EntityManager reflectionsEntityManager;
+
+    @PersistenceContext(name = "suggestedSkills", unitName = "suggestedSkills")
+    private EntityManager suggestedSkillsEntityManager;
+
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<List<Skill>> getSkills() {
         TransactionTemplate transactionTemplate = new TransactionTemplate(transactionManager);
@@ -38,6 +49,8 @@ public class SkillController {
                 return skillRepository.findAllOrderByYearsOfExperience();
             }
         });
+        System.out.println(reflectionsEntityManager.find(Name.class, 1L));
+        System.out.println(suggestedSkillsEntityManager.find(SuggestedSkill.class, "AI"));
         return new ResponseEntity<>(skills, HttpStatus.OK);
     }
 
